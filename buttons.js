e8947a1f89886a34d1b4d3eac40f60ead362c623
cmssetsketch <cmsset@gmail.com>
2025-10-16 11:31:36 +0200

    Add color parameter to makeContinentButton function

--- buttons.js
+++ buttons.js
@@ -1,8 +1,9 @@
 class Button {
-  constructor(x, y, label, onClick, shapes = null, type = "continent") {
+  constructor(x, y, label, col, onClick, shapes = null, type = "continent") {
     this.x = x;
     this.y = y;
     this.label = label;
+    this.col = col;
     this.onClick = onClick;
     this.shapes = shapes;  
     this.type = type; // "continent" or "country"
@@ -11,8 +12,6 @@ class Button {
   this.h = 25;
   this.active = false;
   this.wrappedLabel = null;
-    const rawColor = getCountryColor(label);
-    this.col = color(rawColor);
 }
     
   show(scaleFactor) {
@@ -114,9 +113,9 @@ class Button {
 }
 
 
-function makeContinentButton(x, y, label, shapes) {
+function makeContinentButton(x, y, label, col, shapes) {
   return new Button(
-    x, y, label,
+    x, y, label, col,
     (btn) => {
       selectedContinent = btn.label;
       updateCountriesFromButton(selectedContinent); // show 
@@ -132,44 +131,44 @@ function setupButtons() {
   buttons = [];
 
   buttons.push(
-  makeContinentButton(0, 0, "North America", [
+  makeContinentButton(0, 0, "North America", color("#49c98f"), [
    [ [0,0],[135,0],[135,49],[112,49],[112,73],[88,73],[88,90],[40,90],[40,73],[26,73],[26,49],[0,49]]
   ])
 );  
 
 buttons.push(
-  makeContinentButton(0, 0, "Central America", [
+  makeContinentButton(0, 0, "Central America", color("#8ded4f"), [
    [ [26,73],[40,73],[40,90],[63,90],[63,110],[75,110],[75,121],[26,121]]
   ])
 );  
   
     buttons.push(
-  makeContinentButton(0, 0, "Caribbean", [
+  makeContinentButton(0, 0, "Caribbean", color("#e5f761"), [
    [ [80,96],[116,96],[116,106],[80,106]]
   ])
 );   
 
       buttons.push(
-  makeContinentButton(0, 0, "South America", [
+  makeContinentButton(0, 0, "South America", color("#ffffa1"), [
    [ [75,121],[120,121],[120,136],[147,136],[147,170],[136,170],[136,195],[112,195],[112,218],[95,218],[95,170],[75,170],[75,121]]
   ])
 ); 
 
 buttons.push(
-  makeContinentButton(0, 0, "Nothern Europe", [
+  makeContinentButton(0, 0, "Nothern Europe", color("#25f4f4"), [
  [[208,19],[244,19],[244,45],[208,45]],
   [[185,24],[197,24],[197,36],[185,36]]
   ])
 );    
 
     buttons.push(
-  makeContinentButton(0, 0, "Western Europe", [
+  makeContinentButton(0, 0, "Western Europe", color("#6ddcfc"), [
    [ [198,45],[221,45],[221,57],[213,57],[213,64],[198,64]]
   ])
 ); 
   
 buttons.push(
-  makeContinentButton(0, 0, "Southern Europe", [
+  makeContinentButton(0, 0, "Southern Europe", color("#b6f1ff"), [
         [[185,64],[208,64],[208,77],[185,77],[185,64]],
 
     [[213,57],[221,57],[221,64],[242,64],[242,73],[213,73]]
@@ -177,13 +176,13 @@ buttons.push(
 );  
   
     buttons.push(
-  makeContinentButton(0, 0, "Eastern Europe", [
+  makeContinentButton(0, 0, "Eastern Europe", color("#34bde8"), [
    [ [244,19],[427,19],[427,49],[402,49],[402,61],[382,61],[382,49],[268,49],[268,64],[221,64],[221,45],[244,45]]
   ])
 ); 
 
 buttons.push(
-  makeContinentButton(0, 0, "Eastern Asia", [
+  makeContinentButton(0, 0, "Eastern Asia", color("#4b7cf7"), [
         [[320,49],[382,49],[382,61],[402,61],[402,73],[388,73],[388,97],[353,97],[353,85],[320,85]],
     [[409,67],[419,67],[419,90],[409,90]]
   ])
@@ -191,25 +190,25 @@ buttons.push(
   
 
   buttons.push(
-  makeContinentButton(0, 0, "Central Asia", [
+  makeContinentButton(0, 0, "Central Asia", color("#559aff"), [
    [ [282,49],[320,49],[320,73],[282,73]]
   ])
 );  
 
   buttons.push(
-  makeContinentButton(0, 0, "Southern Asia",  [
+  makeContinentButton(0, 0, "Southern Asia", color("#aa83ff"), [
    [ [282,73],[320,73],[320,85],[341,85],[341,97],[330,97],[330,122],[305,122],[305,92],[282,92]]
   ])
 );  
   
   buttons.push(
-  makeContinentButton(0, 0, "Western Asia", [
+  makeContinentButton(0, 0, "Western Asia", color("#c4b4f9"), [
    [ [242,64],[268,64],[268,73],[282,73],[282,98],[294,98],[294,110],[268,110],[268,98],[257,98],[257,73],[242,73]]
   ])
 );
   
 buttons.push(
-  makeContinentButton(0, 0, "Southeastern Asia", [
+  makeContinentButton(0, 0, "Southeastern Asia", color("#7027e2"), [
     [[341,85],[353,85],[353,97],[379,97],[379,121],[367,121],[367,133],[355,133],[355,110],[341,110]],
 
     [[390,121],[402,121],[402,142],[380,142],[380,130],[390,130]],
@@ -219,25 +218,25 @@ buttons.push(
 );
   
   buttons.push(
-  makeContinentButton(0, 0, "Micronesia", [
+  makeContinentButton(0, 0, "Micronesia", color("#bf00c9"), [
    [ [445,115],[457,115],[457,127],[445,127]]
   ])
 );   
 
   buttons.push(
-  makeContinentButton(0, 0, "Polynesia", [
+  makeContinentButton(0, 0, "Polynesia", color("#f385ff"), [
    [ [487,145],[500,145],[500,158],[487,158]]
   ])
 );   
 
   buttons.push(
-  makeContinentButton(0, 0, "Melanesia", [
+  makeContinentButton(0, 0, "Melanesia",  color("#9e16bf"), [
    [ [422,140],[447,140],[447,152],[422,152]]
   ])
 );  
 
 buttons.push(
-  makeContinentButton(0, 0, "Australia & New Zealand", [
+  makeContinentButton(0, 0, "Australia & New Zealand",  color("#d84ef9"), [
     // first polygon
     [[378,158],[439,158],[439,203],[378,203]],
 
@@ -247,13 +246,13 @@ buttons.push(
 );  
   
   buttons.push(
-  makeContinentButton(0, 0, "Nothern Africa", [
+  makeContinentButton(0, 0, "Nothern Africa", color("#ffd6cf"), [
    [ [175,80],[233,80],[233,87],[257,87],[257,121],[233,121],[233,101],[175,101]]
   ])
 );
 
 buttons.push(
-  makeContinentButton(0, 0, "Eastern Africa", [
+  makeContinentButton(0, 0, "Eastern Africa", color("#f78d94"), [
     // first polygon
     [[269,170],[282,170],[282,189],[269,189]],
 
@@ -264,20 +263,20 @@ buttons.push(
  
   
   buttons.push(
-  makeContinentButton(0, 0, "Middle Africa", [
+  makeContinentButton(0, 0, "Middle Africa", color("#fcad9a"), [
    [ [217,101],[233,101],[233,121],[244,121],[244,157],[233,157],[233,171],[217,171]]
   ])
 );
  
   
   buttons.push(
-  makeContinentButton(0, 0, "Western Africa", [
+  makeContinentButton(0, 0, "Western Africa", color("#ffc4b6"), [
     [[175,101],[217,101],[217,134],[175,134]]
   ])
 );
   
   buttons.push(
-  makeContinentButton(0, 0, "Southern Africa", [
+  makeContinentButton(0, 0, "Southern Africa", color("#ed7f74"), [
     [ [217,171],[244,171],[244,207],[217,207] ]  
     ]));  
 }
